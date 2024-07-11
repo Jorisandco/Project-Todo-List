00e644edb786b10189da6faff69a8f532825ceea
Joris van Rijn <jorisvanrijn2006@gmail.com>
2024-07-11 19:43:19 +0200

    added more functions and variables to the project

--- Projectclass.py
+++ Projectclass.py
@@ -5,22 +5,34 @@ class project:
       self.Projectname = Projectname
       self.projectslist= f"{self.savedata}/savedata.txt"
 
-      file = open(self.projectslist, "r")
-      
+# all the diffrent types of ways to open the project save file
+      self.fileR = open(self.projectslist, "r")
+      self.filew = open(self.projectslist, "w")
+      self.fileA = open(self.projectslist, "a")
+
       backup = open("savedata/backup/backupsavedataPRJList.txt", "w")
-
-      backup.write(file.read())
-
-      print(file.read())
+      backup.write(self.fileR.read())
+      self.fileR.close
 
 
-    def newItem():
+#all the stuff for the project list
+    def displaylist(self):
+        self.fileR
+
+        self.fileR.close()
+    def saveproject(self):
+        self.filew
+
+        self.filew.close()
+
+    #all the things for the project items
+    def newItem(self):
         print("made new item")
-    def removeItem():
+    def removeItem(self):
         print("item removed")
     def setItemTodone():
         print("item finished")
-    def setItemtoWorkingon():
+    def setItemtoWorkingon(self):
         print("you are now working on this item")
-    def SetItemToNotFinished():
+    def SetItemToNotFinished(self):
         print("item is now in not finished list")
